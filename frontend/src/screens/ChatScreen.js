71a99f2b8f2589c486324a9525098329eb807f5a
amir-mansoor <amirmansoor200@gmail.com>
2022-03-23 15:06:16 +0500

    Chat screen

---
+++ frontend/src/screens/ChatScreen.js
@@ -0,0 +1,25 @@
+import {useEffect,useState} from "react"
+import axios from "axios"
+const ChatScreen = () => {
+
+	const [chats,setChats] = useState([])
+
+	const fetchChats = async () =>{
+		const {data} = await axios.get("/api/chat")
+		setChats(data)
+	}
+
+	useEffect(() =>{
+		fetchChats()
+	},[])
+
+	return (
+		<h1>{chats.map((chat) => (
+			<div key={chat._id}>
+				{chat.chatName}
+			</div>
+		))}</h1>
+	)
+}
+
+export default ChatScreen
